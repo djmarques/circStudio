9effe3c65254f36a5e08163a23ef9dd5005f3864
Daniel Marques <fab.dani2030@outlook.com>
2025-06-02 12:08:43 +0100

    update the introduction of the submarine data analysis pipeline

--- circStudio/models/__init__.py
+++ circStudio/models/__init__.py
@@ -1,6 +1,8 @@
 """Module for mathematical modelling of circadian rhythms"""
 
 from .math_models import *
+from .light_tools import Light
+from .tools import *
 
 
-__all__ = [Forger, Jewett, HannaySP, HannayTP, ESRI]
+__all__ = [Forger, Jewett, HannaySP, HannayTP, ESRI, Light, Tools, Cir_Descriptive_Stats]
