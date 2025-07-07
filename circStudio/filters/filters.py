ffd7b1f8f8196ca967afd6dc207d237a1bd89f21
Daniel Marques <fab.dani2030@outlook.com>
2025-07-07 12:00:09 +0100

    Simplify functions related to raw actigraphy files

--- circStudio/filters/filters.py
+++ circStudio/filters/filters.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+import pyexcel as pxl
 import warnings
-from ..log import BaseLog
 
 
 def _create_inactivity_mask(data, duration, threshold):
@@ -62,13 +62,179 @@ def _create_inactivity_mask(data, duration, threshold):
     seq_len = ends - starts
 
     for i in seq_idx[np.where(seq_len >= duration)]:
-        mask[i[0]:i[1]] = 0
+        mask[i[0] : i[1]] = 0
 
     return pd.Series(mask, index=data.index)
 
 
+class BaseLog:
+    """Base class for log files containing time stamps.
+
+    Parameters
+    ----------
+    fname: str
+        Absolute filepath of the input log file.
+    log: pandas.DataFrame
+        Dataframe containing the data found in the log file.
+    """
+
+    def __init__(self, input_fname, log):
+
+        # get absolute file path
+        self.__fname = os.path.abspath(input_fname)
+
+        # Add `duration` column
+        log["Duration"] = log["Stop_time"] - log["Start_time"]
+
+        # Inplace drop of NA
+        log.dropna(inplace=True)
+
+        # add dataframe
+        self.__log = log
+
+    @classmethod
+    def from_file(cls, input_fname, index_name, *args, **kwargs):
+        """Read start/stop-times from log files.
+
+        Generic function to read start and stop times from log files. Supports
+        different file format (.ods, .xls(x), .csv).
+
+        Parameters
+        ----------
+        input_fname: str
+            Path to the log file.
+        index_name: str
+            Name of the index.
+        *args
+            Variable length argument list passed to the subsequent reader
+            function.
+        **kwargs
+            Arbitrary keyword arguments passed to the subsequent reader
+            function.
+
+        Returns
+        -------
+        absname: str
+            Absolute filepath of the input log file.
+        log: pandas.DataFrame
+            Dataframe containing the data found in the log file.
+
+        """
+
+        # get absolute file path
+        absname = os.path.abspath(input_fname)
+
+        # get basename and split it into base and extension
+        basename = os.path.basename(absname)
+        _, ext = os.path.splitext(basename)
+
+        if ext == ".csv":
+            log = cls.__from_csv(absname, index_name, *args, **kwargs)
+        elif (ext == ".xlsx") or (ext == ".xls") or (ext == ".ods"):
+            log = cls.__from_excel(absname, index_name, *args, **kwargs)
+        else:
+            raise ValueError(
+                (
+                    "File format for the input file {}".format(basename)
+                    + "is not currently supported."
+                    + "Supported file format:\n"
+                    + ".csv (text),\n"
+                    + ".ods (OpenOffice spreadsheet),\n"
+                    + ".xls (Excel spreadsheet)."
+                )
+            )
+        return absname, log
+
+    @classmethod
+    def __from_csv(cls, input_fname, index_name, sep=",", dayfirst=False):
+        """Read start/stop-times from .csv files.
+
+        Specific function to read start and stop times from csv files.
+
+        Parameters
+        ----------
+        input_fname: str
+            Path to the log file.
+        index_name: str
+            Name of the index.
+        sep: str, optional
+            Delimiter to use.
+            Default is ','.
+        dayfirst: bool, optional
+            If set to True, use DD/MM/YYYY format dates.
+            Default is False.
+
+        Returns
+        -------
+        log : a pandas.DataFrame
+            A dataframe with the start and stop times (columns)
+
+        """
+
+        # Read data from the csv file into a dataframe
+        log = pd.read_csv(
+            input_fname,
+            sep=sep,
+            dayfirst=dayfirst,
+            header=0,
+            index_col=[0],
+            usecols=[0, 1, 2],
+            names=[index_name, "Start_time", "Stop_time"],
+            parse_dates=[1, 2],
+        )
+        return log
+
+    @classmethod
+    def __from_excel(cls, input_fname, index_name):
+        """Read start/stop-times from excel-like files.
+
+        Specific function to read start and stop times from .ods/.xls(x) files.
+
+        Parameters
+        ----------
+        input_fname: str
+            Path to the log file.
+        index_name: str
+            Name of the index.
+
+        Returns
+        -------
+        log : a pandas.DataFrame
+            A dataframe with the start and stop times (columns)
+
+        """
+
+        # Read data from the log file into a np array
+        sst_narray = np.array(pxl.get_array(file_name=input_fname))
+
+        # Create a DF with columns: index_name, start_time, stop time
+        log = pd.DataFrame(
+            sst_narray[1:, 1:3],
+            index=sst_narray[1:, 0],
+            columns=["Start_time", "Stop_time"],
+            # dtype='datetime64[ns]'
+        )
+        log.index.name = index_name
+
+        return log
+
+    @property
+    def fname(self):
+        """The absolute filepath of the input log file."""
+        return self.__fname
+
+    @property
+    def log(self):
+        """The dataframe containing the data found in the log file."""
+        return self.__log
+
+    def summary(self, colname):
+        """Returns a dataframe of summary statistics."""
+        return self.__log[colname].describe()
+
+
 class FiltersMixin(object):
-    """ Mixin Class """
+    """Mixin Class"""
 
     def create_inactivity_mask(self, duration):
         """Create a mask for inactivity (count equal to zero) periods.
@@ -90,13 +256,13 @@ class FiltersMixin(object):
         if isinstance(duration, int):
             nepochs = duration
         elif isinstance(duration, str):
-            nepochs = int(pd.Timedelta(duration)/self.frequency)
+            nepochs = int(pd.Timedelta(duration) / self.frequency)
         else:
             nepochs = None
             warnings.warn(
-                'Inactivity length must be a int and time offset string (ex: '
-                '\'90min\'). Could not create a mask.',
-                UserWarning
+                "Inactivity length must be a int and time offset string (ex: "
+                "'90min'). Could not create a mask.",
+                UserWarning,
             )
 
         # Store requested mask duration (and discard current mask)
@@ -106,7 +272,7 @@ class FiltersMixin(object):
         self.mask = _create_inactivity_mask(self.activity, nepochs, 1)
 
     def add_mask_period(self, start, stop):
-        """ Add a period to the inactivity mask
+        """Add a period to the inactivity mask
 
         Parameters
         ----------
@@ -127,27 +293,33 @@ class FiltersMixin(object):
             # )
 
         # Check if start and stop are within the index range
-        if (pd.Timestamp(start) < self.mask.index[0]):
-            raise ValueError((
-                "Attempting to set the start time of a mask period before "
-                + "the actual start time of the data.\n"
-                + "Mask start time: {}".format(start)
-                + "Data start time: {}".format(self.mask.index[0])
-            ))
-        if (pd.Timestamp(stop) > self.mask.index[-1]):
-            raise ValueError((
-                "Attempting to set the stop time of a mask period after "
-                + "the actual stop time of the data.\n"
-                + "Mask stop time: {}".format(stop)
-                + "Data stop time: {}".format(self.mask.index[-1])
-            ))
+        if pd.Timestamp(start) < self.mask.index[0]:
+            raise ValueError(
+                (
+                    "Attempting to set the start time of a mask period before "
+                    + "the actual start time of the data.\n"
+                    + "Mask start time: {}".format(start)
+                    + "Data start time: {}".format(self.mask.index[0])
+                )
+            )
+        if pd.Timestamp(stop) > self.mask.index[-1]:
+            raise ValueError(
+                (
+                    "Attempting to set the stop time of a mask period after "
+                    + "the actual stop time of the data.\n"
+                    + "Mask stop time: {}".format(stop)
+                    + "Data stop time: {}".format(self.mask.index[-1])
+                )
+            )
 
         # Set mask values between start and stop to zeros
-        #self.mask.loc[start:stop] = 0
-        self.mask = self.mask.mask((self.mask.index >= start) & (self.mask.index <= stop), 0)
+        # self.mask.loc[start:stop] = 0
+        self.mask = self.mask.mask(
+            (self.mask.index >= start) & (self.mask.index <= stop), 0
+        )
 
     def add_mask_periods(self, input_fname, *args, **kwargs):
-        """ Add periods to the inactivity mask
+        """Add periods to the inactivity mask
 
         Function to read start and stop times from a Mask log file. Supports
         different file format (.ods, .xls(x), .csv).
@@ -165,8 +337,8 @@ class FiltersMixin(object):
         """
 
         # Convert the log file into a DataFrame
-        absname, log = BaseLog.from_file(input_fname, 'Mask', *args, **kwargs)
+        absname, log = BaseLog.from_file(input_fname, "Mask", *args, **kwargs)
 
         # Iterate over the rows of the DataFrame
         for _, row in log.iterrows():
-            self.add_mask_period(row['Start_time'], row['Stop_time'])
+            self.add_mask_period(row["Start_time"], row["Stop_time"])
