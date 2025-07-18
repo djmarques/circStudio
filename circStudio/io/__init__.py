7b95dee377a21cd6cc7d708722f4bcbaa7068113
Daniel Marques <fab.dani2030@outlook.com>
2025-07-18 17:45:36 +0100

    implement mesa adaptor subclass

--- circStudio/io/__init__.py
+++ circStudio/io/__init__.py
@@ -3,5 +3,6 @@ from .atr import read_atr
 from .awd import read_awd
 from .agd import read_agd
 from .dqt import read_dqt
+from .mesa import read_mesa
 
-__all__ = ["Raw", "read_atr", "read_awd", "read_agd", "read_dqt"]
+__all__ = ["Raw", "read_atr", "read_awd", "read_agd", "read_dqt", "read_mesa"]
