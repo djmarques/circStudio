1fafb278003fb927808247ccf5ccabe8e238c842
Daniel Marques <fab.dani2030@outlook.com>
2025-07-18 17:21:41 +0100

    implement dqt reader

--- circStudio/io/__init__.py
+++ circStudio/io/__init__.py
@@ -2,5 +2,6 @@ from .base import Raw
 from .atr import read_atr
 from .awd import read_awd
 from .agd import read_agd
+from .dqt import read_dqt
 
-__all__ = ["Raw", "read_atr", "read_awd", "read_agd"]
+__all__ = ["Raw", "read_atr", "read_awd", "read_agd", "read_dqt"]
