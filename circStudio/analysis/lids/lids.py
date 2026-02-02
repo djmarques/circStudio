b733e5c03c4d4d1f2158249438d3d915cca23056
djmarques <fab.dani2030@outlook.com>
2026-02-02 12:16:54 +0000

    factor code in LIDS implementation - transforms

--- circStudio/analysis/lids/lids.py
+++ circStudio/analysis/lids/lids.py
@@ -8,11 +8,11 @@ from functools import reduce
 from lmfit import fit_report, minimize, Parameters
 from scipy.stats import pearsonr, poisson
 
-
 # Import function for Gaussian smoothing using FWHM
 from smoothing import spm_smooth
 
-
+# Import LID transforms
+from transforms import _lids_func, _lids_inverse_func
 
 def _zero_crossing_points(x):
     """Zero crossing points
@@ -83,45 +83,6 @@ def _inflexion_points(df_dx, d2f_dx2):
     # The first derivative is then used to distinguish between an
     # 'increasing' or 'decreasing' inflexion.
     return _extrema_points(d2f_dx2, df_dx)
-
-
-def _lids_func(x):
-    """LIDS transformation function
-
-    References
-    ----------
-    This code is derived from the original implementation in pyActigraphy, distributed under the BSD 3-Clause License.
-    Original author: Grégory Hammad (gregory.hammad@uliege.be).
-
-    [1] Hammad, G., Reyt, M., Beliy, N., Baillet, M., Deantoni, M., Lesoinne, A., Muto, V., & Schmidt, C. (2021).
-    pyActigraphy: Open-source python package for actigraphy data visualization and analysis.
-    PLoS Computational Biology, 17(10), 1009514–1009535. https://doi.org/10.1371/journal.pcbi.1009514
-
-    [2] Hammad, G., Wulff, K., Skene, D. J., Münch, M., & Spitschan, M. (2024). Open-Source Python Module for the
-    Analysis of Personalized Light Exposure Data from Wearable Light Loggers and Dosimeters.
-    LEUKOS, 20(4), 380–389. https://doi.org/10.1080/15502724.2023.2296863
-    """
-
-    return 100/(x+1)
-
-
-def _lids_inverse_func(x):
-    """LIDS inverse transformation function
-
-    References
-    ----------
-    This code is derived from the original implementation in pyActigraphy, distributed under the BSD 3-Clause License.
-    Original author: Grégory Hammad (gregory.hammad@uliege.be).
-
-    [1] Hammad, G., Reyt, M., Beliy, N., Baillet, M., Deantoni, M., Lesoinne, A., Muto, V., & Schmidt, C. (2021).
-    pyActigraphy: Open-source python package for actigraphy data visualization and analysis.
-    PLoS Computational Biology, 17(10), 1009514–1009535. https://doi.org/10.1371/journal.pcbi.1009514
-
-    [2] Hammad, G., Wulff, K., Skene, D. J., Münch, M., & Spitschan, M. (2024). Open-Source Python Module for the
-    Analysis of Personalized Light Exposure Data from Wearable Light Loggers and Dosimeters.
-    LEUKOS, 20(4), 380–389. https://doi.org/10.1080/15502724.2023.2296863
-    """
-    return 100/x - 1
 
 
 def _lids_pmf(x_lids, mu_lids):
---
+++ circStudio/analysis/lids/transforms.py
@@ -0,0 +1,36 @@
+def _lids_func(x):
+    """LIDS transformation function
+
+    References
+    ----------
+    This code is derived from the original implementation in pyActigraphy, distributed under the BSD 3-Clause License.
+    Original author: Grégory Hammad (gregory.hammad@uliege.be).
+
+    [1] Hammad, G., Reyt, M., Beliy, N., Baillet, M., Deantoni, M., Lesoinne, A., Muto, V., & Schmidt, C. (2021).
+    pyActigraphy: Open-source python package for actigraphy data visualization and analysis.
+    PLoS Computational Biology, 17(10), 1009514–1009535. https://doi.org/10.1371/journal.pcbi.1009514
+
+    [2] Hammad, G., Wulff, K., Skene, D. J., Münch, M., & Spitschan, M. (2024). Open-Source Python Module for the
+    Analysis of Personalized Light Exposure Data from Wearable Light Loggers and Dosimeters.
+    LEUKOS, 20(4), 380–389. https://doi.org/10.1080/15502724.2023.2296863
+    """
+    return 100/(x+1)
+
+
+def _lids_inverse_func(x):
+    """LIDS inverse transformation function
+
+    References
+    ----------
+    This code is derived from the original implementation in pyActigraphy, distributed under the BSD 3-Clause License.
+    Original author: Grégory Hammad (gregory.hammad@uliege.be).
+
+    [1] Hammad, G., Reyt, M., Beliy, N., Baillet, M., Deantoni, M., Lesoinne, A., Muto, V., & Schmidt, C. (2021).
+    pyActigraphy: Open-source python package for actigraphy data visualization and analysis.
+    PLoS Computational Biology, 17(10), 1009514–1009535. https://doi.org/10.1371/journal.pcbi.1009514
+
+    [2] Hammad, G., Wulff, K., Skene, D. J., Münch, M., & Spitschan, M. (2024). Open-Source Python Module for the
+    Analysis of Personalized Light Exposure Data from Wearable Light Loggers and Dosimeters.
+    LEUKOS, 20(4), 380–389. https://doi.org/10.1080/15502724.2023.2296863
+    """
+    return 100/x - 1
